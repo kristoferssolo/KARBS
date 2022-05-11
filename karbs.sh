409e8c4e27b0ce33c4cfa3a2611dc7ea2c4c57c5
Kristofers Solo <kristians.cagulis@gmail.com>
2022-05-11 21:31:35 +0300

    Added solorice install

--- karbs.sh
+++ karbs.sh
@@ -12,12 +12,20 @@ else
   rm -rf paru
 fi
 
-paru -Syu --noconfirm --needed archlinux-keyring - < /pkg-files/"$size"-pkgs.txt
+FILE=pkg-files/"$size"-pkgs.txt
+if [[ -f "$FILE" ]]; then
+  paru -Syu --noconfirm --needed archlinux-keyring - < /pkg-files/"$size"-pkgs.txt
+else
+  wget https://raw.githubusercontent.com/kristoferssolo/karbs/main/pkg-files/"$size"-pkgs.txt
+  paru -Syu --noconfirm --needed archlinux-keyring - < "$size"-pkgs.txt
+fi
 
+mkdir -p ~/repos ~/Downloads ~/Documents ~/Videos ~/Music ~/Pictures/screenshots
+git clone https://github.com/kristoferssolo/solorice ~/repos/solorice
 
-cp -rf .config ~
-cp -rf .local ~
-cp -f .cofig/zsh/.zshenv ~
+cp -rf ~/repos/solorice/.config ~
+cp -rf ~/repos/solorice/.local ~
+cp -f ~/repos/solorice/.cofig/zsh/.zshenv ~
 git clone https://github.com/streetturtle/awesome-wm-widgets ~/.config/awesome/awesome-wm-widgets
 
-mkdir ~/Pictures ~/Documents ~/Videos ~/Downloads ~/Music
+
