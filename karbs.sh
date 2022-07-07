2e50b40926c80a55130e4b17597eacfc3c17cd22
Kristofers Solo <kristians.cagulis@gmail.com>
2022-07-07 13:56:40 +0300

    Changed :: Synchronizing package databases...  system downloading...  world downloading...  galaxy downloading...  lib32 downloading...  extra downloading...  community downloading...  multilib downloading... :: Starting full system upgrade...  there is nothing to do :: Looking for AUR upgrades... :: Looking for devel upgrades... :: orphans: speedtest-rs  there is nothing to do to

--- karbs.sh
+++ karbs.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 
-echo "Choose installation size: minimal or full"
+echo 'Choose installation size: minimal or full'
 read size
 
-if pacman -Q paru; then echo
+if pacman -Q paru; then
+	echo
 else
-  git clone https://aur.archlinux.org/paru
-  cd paru
-  makepkg -si
-  cd ..
-  rm -rf paru
+	git clone 'https://aur.archlinux.org/paru-bin'
+	cd paru
+	makepkg -si
+	cd ..
+	rm -rf paru
 fi
 
-FILE=pkg-files/"$size"-pkgs.txt
+FILE = "pkg-files/$size-pkgs.txt"
+
+paru -Syu --noconfirm --needed archlinux-keyring
+
 if [[ -f "$FILE" ]]; then
-  paru -Syu --noconfirm --needed archlinux-keyring - < /pkg-files/"$size"-pkgs.txt
+	paru -S --noconfirm --needed - <"pkg-files/$size-pkgs.txt"
 else
-  curl -LO https://raw.githubusercontent.com/kristoferssolo/karbs/main/pkg-files/"$size"-pkgs.txt
-  paru -Syu --noconfirm --needed archlinux-keyring - < "$size"-pkgs.txt
-  rm "$size"-pkgs.txt
+	curl -LO "https://raw.githubusercontent.com/kristoferssolo/karbs/main/pkg-files/$size-pkgs.txt"
+	paru -S --noconfirm --needed - <"$size-pkgs.txt"
+	rm "$size"-pkgs.txt
 fi
 
-mkdir -p ~/repos ~/Downloads ~/Documents ~/Videos ~/Music ~/Pictures/screenshots
-git clone https://github.com/kristoferssolo/solorice ~/repos/solorice
+mkdir -p ~/{repos,Downloads,Documents,Videos,Music,Pictures/screenshots}
+git clone 'https://github.com/kristoferssolo/solorice' '~/repos/solorice'
 
-cp -rf ~/repos/solorice/.config ~
-rm -rf ~/.config/awesome/desktop
-cp -rf ~/repos/solorice/.local ~
-mv ~/.config/zsh/.zshenv ~
-git clone https://github.com/streetturtle/awesome-wm-widgets ~/.config/awesome/awesome-wm-widgets
-
-
+cp -rf '~/repos/solorice/.config' ~
+rm -rf '~/.config/awesome/desktop'
+cp -rf '~/repos/solorice/.local' ~
+mv '~/.config/zsh/.zshenv' ~
+git clone 'https://github.com/streetturtle/awesome-wm-widgets' '~/.config/awesome/awesome-wm-widgets'
